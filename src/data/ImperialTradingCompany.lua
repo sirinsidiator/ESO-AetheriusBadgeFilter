9f3bfa01e4efe69eb23f3d80b8912da8918fd524
sirinsidiator <insidiator@cmos.at>
2016-06-28 04:53:40 +0200

    added data for the Imperial Trading Company

---
+++ src/data/ImperialTradingCompany.lua
@@ -0,0 +1,182 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Imperial Trading Company", {
+    {
+        name = "Trading",
+        description = "Badges that are awarded to members for their economic exploits. These badges will be used as a basis for promotions to Trader and Merchant rank",
+        badges = {
+            {
+                name = "Top Trader",
+                description = "Has been one of the top 5 sellers of last week. Will be removed after a week",
+                color = "A4A578",
+            },
+            {
+                name = "Vendor",
+                description = "Has sold wares for more than 200k gold during a week. Can be awarded multiple times",
+                color = "80A36B",
+            },
+            {
+                name = "Shopkeeper",
+                description = "Has received 10 Vendor badges. Can be awarded multiple times",
+                color = "46A085",
+            },
+            {
+                name = "Entrepreneur",
+                description = "Has been one of the top 5 sellers in the past. Can be awarded multiple times",
+                color = "74739E",
+            },
+            {
+                name = "Executive",
+                description = "Has received 10 Entrepreneur badges. Can be awarded multiple times",
+                color = "996380",
+            },
+            {
+                name = "Tycoon",
+                description = "Has either received 10 Shopkeeper badges or 10 Executive badges. Can be awarded multiple times",
+                color = "B55A05",
+            },
+        }
+    },
+    {
+        name = "Participation",
+        description = "Badges that are awarded to members for their activities in the guild. These badges will earn you much appreciation and are exchanged for a present on each new DLC launch",
+        badges = {
+            {
+                name = "Officer",
+                description = "An active officer of the Imperial Trading Company. Contact them if you have questions, suggestions, problems, etc.",
+                color = "FF0000",
+            },
+            {
+                name = "Benefactor",
+                description = "Has donated more than 5k gold in a week. Can be awarded multiple times",
+                color = "D87C2F",
+            },
+            {
+                name = "Philanthropist",
+                description = "Has helped members of the guild with knowledge, action or coin. Can be awarded multiple times",
+                color = "64BCB3",
+            },
+            {
+                name = "Participant",
+                description = "Has joined in a guild activity organized by the management. Can be awarded multiple times",
+                color = "6DD374",
+            },
+            {
+                name = "Associate",
+                description = "Has signed up on the Imperial Trading Company forums (www.imperialtradingcompany.net) and written 10 posts",
+                color = "A37AD6",
+            },
+--            {
+--                name = "Away",
+--                description = "Is currently unavailable and will be back at the date specified next to the badge",
+--                color = "AAAAAA",
+--            },
+--            {
+--                name = "Do Not Kick",
+--                description = "Shall always stay with the Imperial Trading Company",
+--                color = "78B764",
+--            },
+        }
+    },
+    {
+        name = "Crafting",
+        description = "Badges that identify crafters that take commissions. To get one for yourself, contact an officer",
+        badges = {
+            {
+                name = "Blacksmith",
+                description = "Creates weapons or heavy armor",
+                color = "375F7A",
+            },
+            {
+                name = "Clothier",
+                description = "Creates light or medium armor",
+                color = "A52B5E",
+            },
+            {
+                name = "Woodworker",
+                description = "Creates staves or shields",
+                color = "AF6438",
+            },
+            {
+                name = "Alchemist",
+                description = "Brews potions or poisons",
+                color = "349118",
+            },
+            {
+                name = "Enchanter",
+                description = "Enchants glyphs",
+                color = "A44DBC",
+            },
+            {
+                name = "Provisioner",
+                description = "Cooks food or brews drinks",
+                color = "A83E39",
+            },
+            {
+                name = "Jack of all Trades",
+                description = "Can craft items in any category",
+                color = "AD8D1B",
+            },
+            {
+                name = "Trait Master",
+                description = "Can provide research materials",
+                color = "3FC6C6",
+            },
+        }
+    },
+    {
+        name = "Adventuring",
+        description = "Dungeon related badges. To get one for yourself, contact an officer",
+        badges = {
+            {
+                name = "Tank",
+                description = "Takes the role of a tank in dungeons",
+                color = "5589B5",
+            },
+            {
+                name = "Healer",
+                description = "Takes the role of a healer in dungeons",
+                color = "59A03B",
+            },
+            {
+                name = "Damage Dealer",
+                description = "Takes the role of a damage dealer in dungeons",
+                color = "B76856",
+            },
+        }
+    },
+    {
+        name = "Bites",
+        description = "Badges for Vampires and Werewolves that will bite you for free. To get one for yourself, contact an officer",
+        badges = {
+            {
+                name = "Werewolf AD",
+                description = "Can give you a free Werewolf bite in the Aldmeri Dominion",
+                color = "CD943C",
+            },
+            {
+                name = "Werewolf EP",
+                description = "Can give you a free Werewolf bite in the Ebonheart Pact",
+                color = "DA6D3F",
+            },
+            {
+                name = "Werewolf DC",
+                description = "Can give you a free Werewolf bite in the Daggerfall Covenant",
+                color = "937F76",
+            },
+            {
+                name = "Vampire AD",
+                description = "Can give you a free Vampire bite in the Aldmeri Dominion",
+                color = "CD6D6B",
+            },
+            {
+                name = "Vampire EP",
+                description = "Can give you a free Vampire bite in the Ebonheart Pact",
+                color = "DA456D",
+            },
+            {
+                name = "Vampire DC",
+                description = "Can give you a free Vampire bite in the Daggerfall Covenant",
+                color = "9358A4",
+            },
+        }
+    },
+})
