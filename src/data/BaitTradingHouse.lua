77951d53640a831c3f8e5d0bcbbb4d0e47be7be8
sirinsidiator <insidiator@cmos.at>
2016-09-08 16:33:38 +0200

    added data for Bait Trading House

---
+++ src/data/BaitTradingHouse.lua
@@ -0,0 +1,124 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Bait Trading House", {
+    {
+        name = "Soziales",
+        badges = {
+            {
+                name = "Uralter",
+                description = "Gehoert zu den Gruendungsmitgliedern.",
+            },
+            {
+                name = "Althase",
+                description = "Ist seit mindestens 200 Tagen in der Gilde.",
+            },
+            {
+                name = "Veteran",
+                description = "Ist seit mindestens 100 Tagen in der Gilde",
+            },
+            {
+                name = "Grossherziger Spender",
+                description = "Hat bereits ueber 100k gespendet.",
+            },
+        }
+    },
+    {
+        name = "Handel",
+        badges = {
+            {
+                name = "Meisterhaendler",
+                description = "War Topverkaeufer in einer Handelswoche. Diese Auszeichnung kann mehrfach vergeben werden.",
+            },
+            {
+                name = "Ruhm und Ehre",
+                description = "Ruhm und Ehre fuer die, die 10 Meisterhaendler-Auszeichnungen erhalten haben.",
+            },
+        }
+    },
+    {
+        name = "Events",
+        badges = {
+            {
+                name = "Aktiver",
+                description = "Hat an Gildenevents teilgenommen. Diese Auszeichnung kann mehrfach vergeben werden.",
+            },
+            {
+                name = "Glueckskind",
+                description = "Hat in einer Lotterie gewonnen.",
+            },
+            {
+                name = "Angelprofi",
+                description = "Hat in einem Angelevent gewonnen.",
+            },
+            {
+                name = "Unermuedlicher Sucher",
+                description = "Hat im Versteckspiel gewonnen.",
+            },
+        }
+    },
+    {
+        name = "Handwerk",
+        badges = {
+            {
+                name = "Handwerksmeister",
+                description = "Beherrscht alle Handwerke",
+                relations = {"Alchemist", "Meisterkoch", "Schreiner", "Schneider", "Schmied", "Verzauberer"},
+            },
+            {
+                name = "Alchemist",
+                description = "Beherrscht alle Zutaten und hat alle Stufen ausgelevelt.",
+            },
+            {
+                name = "Meisterkoch",
+                description = "Kann mehr als 90% aller Rezepte und kocht dir die verzueglichsten Lebensmittel.",
+            },
+            {
+                name = "Schmied",
+                description = "Kann alle 9 Traits der Schmiedekunst und hat das Handwerk ausgelevelt.",
+            },
+            {
+                name = "Schneider",
+                description = "Kann alle 9 Traits der Schneiderei und hat das Handwerk ausgelevelt.",
+            },
+            {
+                name = "Schreiner",
+                description = "Kann alle 9 Traits der Schreinerei und hat das Handwerk ausgelevelt.",
+            },
+            {
+                name = "Verzauberer",
+                description = "Beherrscht alle Runentypen und hat das Handwerk ausgelevelt.",
+            },
+            {
+                name = "Stilsicher",
+                description = "Beherrscht alle derzeitigen Stile.",
+            },
+        }
+    },
+    {
+        name = "PVE und PVP Kontakte",
+        badges = {
+            {
+                name = "AD PVE Gott",
+                description = "Ist Ansprechpartner fuer eine PVE-Gilde im Aldmeri Dominion",
+            },
+            {
+                name = "AD PVP Gott",
+                description = "Ist Ansprechpartner fuer eine PVP-Gilde im Aldmeri Dominion",
+            },
+            {
+                name = "DC PVE Gott",
+                description = "Ist Ansprechpartner fuer eine PVE-Gilde im Dolchsturzbündnis",
+            },
+            {
+                name = "DC PVP Gott",
+                description = "Ist Ansprechpartner fuer eine PVP-Gilde im Dolchsturzbündnis",
+            },
+            {
+                name = "EP PVE Gott",
+                description = "Ist Ansprechpartner fuer eine PVE-Gilde im Ebenherzpakt",
+            },
+            {
+                name = "EP PVP Gott",
+                description = "Ist Ansprechpartner fuer eine PVP-Gilde im Ebenherzpakt",
+            },
+        }
+    }
+})
