12799b038fa41dd4e1a7e873441646240cbaf7b0
sirinsidiator <insidiator@cmos.at>
2016-09-14 11:19:36 +0200

    added data for Zenithar's Trading House

---
+++ src/data/ZenitharsTradingHouse.lua
@@ -0,0 +1,116 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Zenithar's Trading House", {
+    {
+        name = "Crafting Badges",
+        badges = {
+            {
+                name = "Craft God",
+                description = "Has mastered every crafting discipline and can craft every single item in each of them.",
+                relations = {"Master Alchemist", "Master Chef", "Master Crafter", "Master Enchanter", "Master Stylist"},
+            },
+            {
+                name = "Master Alchemist",
+                description = "Has mastered alchemy and can mix the most powerful potions and poisons.",
+            },
+            {
+                name = "Master Chef",
+                description = "Has learned more than 90% of all recipes, can cook the most delicious food and brew the most refreshing drinks.",
+            },
+            {
+                name = "Master Crafter",
+                description = "Has researched all 9 traits for every single item type.",
+            },
+            {
+                name = "Master Enchanter",
+                description = "Has knowledge of all rune types and can craft the best glyphs.",
+            },
+            {
+                name = "Master Stylist",
+                description = "Has discovered all the different crafting styles in the game.",
+            },
+        }
+    },
+    {
+        name = "Social Badges",
+        badges = {
+            {
+                name = "Activist",
+                description = "Has participated in a guild event, poll or similar.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Ancient One",
+                description = "Has been in the guild for more than one year.",
+            },
+            {
+                name = "Gold Member",
+                description = "Has been in the guild since its foundation.",
+            },
+            {
+                name = "Gold'n'Glory",
+                description = "Has donated more than 100k gold to the guild bank.",
+            },
+            {
+                name = "Veteran",
+                description = "Has been in the guild for more than 30 weeks.",
+            },
+            {
+                name = "VIP",
+                description = "Has aided the guild or community in a special way. Has a different set of rules applied. You can only get this badge from officers.",
+            },
+        }
+    },
+    {
+        name = "Event Badges",
+        badges = {
+            {
+                name = "Connoisseur",
+                description = "Has participated in any \"Sight of the Elders\" event.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Dirty RP",
+                description = "Has participated in any RP event.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Gladiator",
+                description = "Has participated in any PvP event.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Seeker",
+                description = "Has participated in any \"Riddler's Path\" event.\nCan be awarded multiple times.",
+            },
+        }
+    },
+    {
+        name = "Trade Badges",
+        badges = {
+            {
+                name = "Favorite of Zenithar",
+                description = "Has been blessed by Zenithar in reward for earnest work and honest profit.\nHas earned 'Blessed by Gold' rank.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Master Trader",
+                description = "Has earned the guild rank 'Master Trader'.\nCan be awarded multiple times.",
+            },
+            {
+                name = "Platinum Trader",
+                description = "Has earned the 'Master Trader' badge 10 times.\nCan be awarded multiple times.",
+            },
+        },
+    },
+    {
+        name = "Utility Badges",
+        badges = {
+            {
+                name = "Notification",
+                description = "Has broken guild rules. A notification has been sent via mail.",
+            },
+            {
+                name = "Novice",
+                description = "New in guild.",
+            },
+            {
+                name = "Way Out",
+                description = "Has ignored the guild rules and all notifications. Will be kicked if we need space for new members.",
+            },
+        },
+    },
+})
