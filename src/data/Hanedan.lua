53c9a5fd1c01af1fb1c57b1dbb07634f374e976a
sirinsidiator <insidiator@cmos.at>
2016-11-22 18:44:32 +0100

    added data for Hanedan

---
+++ src/data/Hanedan.lua
@@ -0,0 +1,96 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Hanedan", {
+    {
+        name = "PvE Ranks",
+        badges = {
+            {
+                name = "Raid Leader",
+                description = "Raidleri yoneten kisi",
+            },
+            {
+                name = "Core Raider",
+                description = "Kemik kadroda bulunan kisi",
+            },
+            {
+                name = "Raider",
+                description = "Raid oyuncusu",
+            },
+        }
+    },
+    {
+        name = "PvP Ranks",
+        badges = {
+            {
+                name = "Zerg Leader",
+                description = "Zerg yoneticisi",
+            },
+            {
+                name = "Zerg Member",
+                description = "Zerg uyesi",
+            },
+            {
+                name = "Hunter",
+                description = "PvP oyuncusu",
+            },
+        }
+    },
+    {
+        name = "Social Ranks",
+        badges = {
+            {
+                name = "Founder",
+                description = "Guildin kurulumunda emegi gecen yuce insanlar",
+            },
+            {
+                name = "Officer",
+                description = "Mecliste bulunan yetkili uyeler",
+            },
+            {
+                name = "Class Master",
+                description = "Karakterler hakkinda bilgi sahibi oyuncular",
+            },
+            {
+                name = "Active Member",
+                description = "Aktif olarak guild etkinliklerine katilan uyeler",
+            },
+        }
+    },
+    {
+        name = "Event Rank",
+        badges = {
+            {
+                name = "Winner",
+                description = "Etkinlik kazanan uye",
+            },
+            {
+                name = "Former Winner",
+                description = "Daha once etkinlik kazanan uye",
+            },
+        }
+    },
+    {
+        name = "Craft Ranks",
+        badges = {
+            {
+                name = "Master Crafter",
+                description = "Her tur crafta hakim ve uretebilen kisiler",
+                relations = {"Master Blacksmith", "Master Clothier", "Master Woodworker", "Master Chef"},
+            },
+            {
+                name = "Master Blacksmith",
+                description = "Blacksmithingte 9 trait sahibi kisiler",
+            },
+            {
+                name = "Master Clothier",
+                description = "Clothingte 9 trait sahibi kisiler",
+            },
+            {
+                name = "Master Woodworker",
+                description = "Woodworkingte 9 trait sahibi kisiler",
+            },
+            {
+                name = "Master Chef",
+                description = "Her yemegi yapabilen kisiler",
+            },
+        },
+    }
+})
