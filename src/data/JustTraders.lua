2adcd1615676d98ddac30568220b4fe66e707c2d
Arkadius <ghetto@freenet.de>
2017-02-01 22:51:36 +0100

    updated data for Just Traders

--- src/data/JustTraders.lua
+++ src/data/JustTraders.lua
@@ -31,11 +31,21 @@ AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Just Traders
     {
         name = "Trading",
         badges = {
+		
+		   {
+                name = "Aspirant",
+                description = "Reached 500k sales/week. Can be awarded multiple times.",
+            },
 
             {
                 name = "Glorious",
                 description = "Reached 1kk sales/week. Can be awarded multiple times.",
-            },            
+            },
+			{
+                name = "Premium Trader",
+                description = "Reached 1kk sales/week 10 times.",
+            },
+		            
             {
                 name = "Master Trader",
                 description = "Has been Top-Trader in one trading week. Can be awarded multiple times.",
@@ -46,42 +56,5 @@ AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Just Traders
             },
         }
     },
-    {
-        name = "Crafting",
-        badges = {
-            {
-                name = "Master Crafter",
-                description = "Mastered Alchemy, Enchanting, Provisioning, Backsmithing, Clothing and Woodworking",
-                relations = {"Alchemist", "Provisioner", "Woodworker", "Tailor", "Blacksmith", "Enchanter"},
-            },
-            {
-                name = "Alchemist",
-                description = "Knows all ingredients and reached lvl 50 in Alchemy",
-            },
-            {
-                name = "Provisioner",
-                description = "Knows 90% of all recipes and mastered Provisioning",
-            },
-            {
-                name = "Blacksmith",
-                description = "Knows all 9 traits on heavy armor and weapons and reached lvl 50 in Blacksmithing.",
-            },
-            {
-                name = "Tailor",
-                description = "Knows all 9 traits on light and medium armor and reached lvl 50 in Clothing.",
-            },
-            {
-                name = "Woodworker",
-                description = "Knows all 9 traits on shields and wooden weapons and reached lvl 50 in Woodworking.",
-            },
-            {
-                name = "Enchanter",
-                description = "Knows all runes and reached lvl 50 in Enchanting.",
-            },
-            {
-                name = "Karl Lagerfeld",
-                description = "Knows all recent crafting styles.",
-            },
-        }
-    },
+   
 })
