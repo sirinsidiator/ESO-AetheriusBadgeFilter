2e957d5bfe1ecfdff5c1db592c245609ba86d5b2
Arkadius <ghetto@freenet.de>
2017-04-22 16:06:07 +0200

    added data for Haus Winterfell

---
+++ src/data/HausWinterfell.lua
@@ -0,0 +1,100 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Haus Winterfell", {
+    {
+        name = "SOZIALES",
+        badges = {
+            {
+                name = "Methusalem",
+                description = "300 Tage in der Gilde",
+            },
+            {
+                name = "Ältester",
+                description = "200 Tage in der Gilde",
+            },
+            {
+                name = "Veteran",
+                description = "	100 Tage in der Gilde",
+            },
+		    {
+                name = "Meisterangler",
+            },
+			{
+                name = "Höhlenforscher",
+            },
+            {
+                name = "Pionier",
+            },
+            {
+                name = "Unterkunft",
+                description = "Gewährt allen Einlass in sein Haus",
+            },
+            {
+                name = "Himmelsscherbenjäger",
+            },
+            {
+                name = "Vampir",
+            },
+			{
+                name = "Werwolf",
+            },
+        }
+    },
+    {
+        name = "HANDEL",
+        badges = {
+            {
+                name = "Midas",
+                description = "Bester Händler einer Handelswoche (kann mehrfach vergeben werden)",
+            },
+            {
+                name = "Krösus",
+                description = "500k in einer Handelswoche umgesetzt (kann mehrfach vergeben werden)",
+            },
+            {
+                name = "Krämer",
+                description = "250k in einer Handelswoche umgesetzt (kann mehrfach vergeben werden)",
+            },
+        }
+    },
+    {
+        name = "HANDWERK",
+        badges = {
+            {
+                name = "Meister aller Stile",
+                description = "Beherrscht alle Stile",
+            },
+            {
+                name = "Meister aller Handwerke",
+                description = "Hat alle Handwerke auf dem Maximum",
+            },
+            {
+                name = "Meisterkoch",
+                description = "Beherrscht alle Rezepte",
+            },
+            {
+                name = "Verrückter Architekt",
+                description = "Beherrscht alle Blaupausen",
+            },
+            {
+                name = "Meisteranalytiker",
+                description = "Hat alle Traits analysiert",
+            },
+        },
+    },
+    {
+        name = "PVP RÄNGE",
+        badges = {
+            {
+                name = "Unteroffizier",
+                description = "Rang 11 im PVP",
+            },
+            {
+                name = "Leutnant",
+                description = "Rang 17 im PVP",
+            },
+            {
+                name = "Oberst",
+                description = "Rang 25 im PVP",
+            },
+        }
+    }
+})
