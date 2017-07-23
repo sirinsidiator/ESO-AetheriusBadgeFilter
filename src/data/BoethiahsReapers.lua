edfa4c3399912afa7166909981e22fc2b79d93cd
sirinsidiator <insidiator@cmos.at>
2017-07-23 17:50:24 +0200

    added data for Boethiah's Reapers

---
+++ src/data/BoethiahsReapers.lua
@@ -0,0 +1,119 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Boethiah's Reapers", {
+    {
+        name = "Hall of honor",
+        description = "Badges that are awarded to members for their tournament exploits.",
+        badges = {
+            {
+                name = "Grand Champion",
+                description = "Reigning champion of dueling tournament",
+                color = "ff0000",
+            },
+            {
+                name = "Ex Champion",
+                description = "Honored ex-champion one of dueling tournaments",
+                color = "800080",
+            },
+            {
+                name = "Participant",
+                description = "Participant one of dueling tournaments",
+                color = "191970",
+            },
+        }
+    },
+    {
+        name = "Guild",
+        description = "Badges that are awarded to members for their activities in the guild.",
+        badges = {
+            {
+                name = "Officer",
+                description = "An active officer of the guild. Contact them if you have questions, suggestions, problems, etc.",
+                color = "cc0000",
+            },
+            {
+                name = "GuildHall",
+                description = "You can visit the guild hall by right clicking on @sh4d0ff on the guild roster and selecting 'Visit players primary residence'",
+                color = "e0301e",
+            },
+            {
+                name = "Arena Master",
+                description = "Members who provide their primary residence as dueling arena for the guild",
+                color = "dc6900",
+            },
+            {
+                name = "Crafter",
+                description = "Crafters who can craft equipments/consumables for you and provide items for research",
+                color = "eb8c00",
+            },
+        }
+    },
+    {
+        name = "Alliance",
+        description = "Your allience preferences in a AvA. To get badge for yourself, contact an officer.",
+        badges = {
+            {
+                name = "Aldmeri Dominion",
+                color = "daa520",
+            },
+            {
+                name = "Ebonhearth Pact",
+                color = "800000",
+            },
+            {
+                name = "Daggerfall Covenant",
+                color = "003366",
+            },
+        }
+    },
+    {
+        name = "Roles",
+        description = "Your role preferences in a group. To get badge for yourself, contact an officer.",
+        badges = {
+            {
+                name = "PvE-Tank",
+                description = "Takes the role of a tank in dungeons",
+                color = "005b96",
+            },
+            {
+                name = "PvE-Damage Dealer",
+                description = "Takes the role of a damage dealer in dungeons",
+                color = "6497b1",
+            },
+            {
+                name = "PvE-Healer",
+                description = "Takes the role of a healer in dungeons",
+                color = "b3cde0",
+            },
+            {
+                name = "PvP-Tank",
+                description = "Takes the role of a tank in battlegrounds or alliance war",
+                color = "8ae429",
+            },
+            {
+                name = "PvP-Damage Dealer",
+                description = "Takes the role of a damage dealer in battlegrounds or alliance war",
+                color = "9afe2e",
+            },
+            {
+                name = "PvP-Healer",
+                description = "Takes the role of a healer in battlegrounds or alliance war",
+                color = "aefe57",
+            },
+        }
+    },
+    {
+        name = "Bites",
+        description = "Badges for Vampires and Werewolves that will bite you for free. To get one for yourself, contact an officer.",
+        badges = {
+            {
+                name = "Werewolf",
+                description = "Can give you a free Werewolf bite",
+                color = "4d7f17",
+            },
+            {
+                name = "Vampire",
+                description = "Can give you a free Vampire bite",
+                color = "4c516d",
+            },
+        }
+    },
+})
