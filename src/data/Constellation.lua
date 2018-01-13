2c263027cfe61fd986b30c7429458151ef014b30
sirinsidiator <insidiator@cmos.at>
2018-01-13 20:20:34 +0100

    added data for Constellation

---
+++ src/data/Constellation.lua
@@ -0,0 +1,83 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Constellation", {
+    {
+        name = "Guild Activity",
+        description = "Badges associated with tracking guild Activity.",
+        badges = {
+            {
+                name = "AFK",
+                description = "Given to people that notify Officership about shorter absence from the game, they won't be kicked for up to 3 months from when their AFK note starts.",
+                color = "779cff",
+            },
+            {
+                name = "AWL",
+                description = "Away with Leave. Given to people that notify Officership about longer absence from the game, they won't be kicked for extended period of time.",
+                color = "779cff",
+            },
+            {
+                name = "Veteran",
+                description = "Given to people that have been in the guild for longer than 28 days.",
+                color = "cc66cc",
+            },
+            {
+                name = "Loyalist",
+                description = "Given to people that have been in the guild for longer than 365 days.",
+                color = "cc66cc",
+            },
+        }
+    },
+    {
+        name = "Donations and Trading",
+        description = "Badges associated with tracking donations and trading.",
+        badges = {
+            {
+                name = "Donated",
+                description = " Shows how much a person has donated to the guild bank.",
+                color = "FFF300",
+                limit = false,
+            },
+            {
+                name = "Golden",
+                description = " Awarded to people who have donated over 100k to the guild bank.",
+                color = "FFCC00",
+            },
+            {
+                name = "Star Trader",
+                description = " Awarded at end of sales week, is given to those who have over 100k in sales in a week.",
+                color = "33ff33",
+                limit = false,
+            },
+        }
+    },
+    {
+        name = "Crafting",
+        description = "Badges associated with crafting.",
+        badges = {
+            {
+                name = "Master Crafter",
+                description = " Given to people that notify Officership that they have a crafter with every trait researched.",
+                color = "993300",
+            },
+            {
+                name = "Station at Home",
+                description = "Given to people that notify Officership that they own either a set of attuned crafting stations or Specialty station at home.",
+                color = "FF8C00",
+            },
+        }
+    },
+    {
+        name = "Bites",
+        description = "Badges associated with availability of Bites.",
+        badges = {
+            {
+                name = "Vampire",
+                description = "Can give vampire bites for free.",
+                color = "990000",
+            },
+            {
+                name = "Werewolf",
+                description = "Can give werewolf bites for free.",
+                color = "663333",
+            },
+        }
+    },
+})
