3460363b0d6118fdbec893317b3597ceeba9314b
sirinsidiator <insidiator@cmos.at>
2020-05-28 22:10:43 +0200

    updated data for Aetherius Trade

--- src/data/AetheriusTrade.lua
+++ src/data/AetheriusTrade.lua
@@ -1,108 +1,45 @@
 AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Aetherius Trade", {
     {
-        name = "Crafting Badges",
+        name = "Trade Badges",
         badges = {
             {
-                name = "Craft God",
-                description = "Has mastered every crafting discipline and can craft every single item in each of them.",
+                name = "Powerseller",
+                description = "Obtained \"Legend\" Rank 2 weeks in a row.\nWill be removed after obtaining \"Broke AF\" rank 2 weeks in a row.",
+                color = "FFFF00",
             },
-            {
-                name = "Artisan",
-                description = "Ready to craft items(armor, weapon, consumables etc) for gold.",
-            },
-        }
-    },
-    {
-        name = "Social Badges",
-        badges = {
-            {
-                name = "Activist",
-                description = "Has participated in a guild event, poll or similar. Can be awarded multiple times.",
-            },
-            {
-                name = "Veteran",
-                description = "Has been in the guild for more than 30 weeks.",
-            },
-            {
-                name = "Ancient One",
-                description = "Has been in the guild for more than one year.",
-            },
-            {
-                name = "Pillar of Zenithar",
-                description = "Has been in the guild for more than three years.",
-            },
-            {
-                name = "Gold Member",
-                description = "Has been in the guild since its foundation.",
-            },
-            {
-                name = "Guild's Pride",
-                description = "Has aided the guild or community in a special way. Has a different set of rules applied. You can only get this badge from officers.",
-            },
-        }
+        },
     },
     {
         name = "Support Badges",
         badges = {
             {
-                name = "Founder",
-                description = "Great Maecenas. Providing awesome support for guild.",
+                name = "Friend of AT",
+                description = "Obtained by helping out the guild, can only be given by GM's.",
+                color = "7EFDFF",
             },
             {
-                name = "Gold'n'Glory",
-                description = "Has donated more than 100k gold to the guild bank.",
-            },
-            {
-                name = "Helpful Hand",
-                description = "Has donated more than 250k gold to the guild bank.",
-            },
-            {
-                name = "Golden Mark",
-                description = "Has donated more than 500k gold to the guild bank.",
-            },
-            {
-                name = "Golden Support",
-                description = "Has donated more than 1m gold to the guild bank.",
+                name = "Friend of ATA",
+                description = "Obtained by helping out the guild, can only be given by GM's.",
+                color = "7EFDFF",
             },
         }
-    },
-    {
-        name = "Trade Badges",
-        badges = {
-            {
-                name = "Perfect Merchant",
-                description = "52 trading weeks or more at February 2018 start. * for twice.",
-                limit = false,
-            },
-            {
-                name = "Favorite of Zenithar",
-                description = "Has been blessed by Zenithar in reward for earnest work and honest profit.\nHas earned 'Blessed by Gold' rank.\nCan be awarded multiple times.",
-                limit = false,
-            },
-            {
-                name = "Master Trader",
-                description = "Has earned the guild rank 'Master Trader'.\nCan be awarded multiple times.",
-            },
-            {
-                name = "Platinum Trader",
-                description = "Has earned the 'Master Trader' badge 10 times.",
-            },
-            {
-                name = "Diamond Trader",
-                description = "Has earned the 'Master Trader' badge 50 times.",
-            },
-        },
     },
     {
         name = "Utility Badges",
         badges = {
             {
-                name = "Notification",
-                description = "Has broken guild rules. A notification has been sent via mail.",
+                name = "Officer",
+                description = "Guild officer (stating the obvious here)",
+                color = "FF1493",
             },
             {
-                name = "Novice",
-                description = "New in guild.",
+                name = "Warned for Chat Rules",
+                description = "People who broke chat rules - read MOTD for these Rules.",
+                color = "FF0000",
+            },
+            {
+                name = "Offline Play",
+                description = "People who play on Offline Mode for a longer period of Time. Ask GM for more info.",
             },
         },
     },
