627ad767dac6719a24e4d3cc2ab303a4e1ff3cd9
sirinsidiator <insidiator@cmos.at>
2020-11-07 22:13:22 +0100

    added data for The Misadventurers League

---
+++ src/data/TheMisadventurersLeague.lua
@@ -0,0 +1,100 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "The Misadventurers League", {
+    {
+        name = "Organisational",
+        description = "Badges used to keep track of various things related to the management of the guild",
+        badges = {
+            {
+                name = "Guild Master",
+                description = "Supreme Being",
+                color = "ffff00",
+            },
+            {
+                name = "Assistant Guild Master",
+                description = "Supreme Being",
+                color = "ffff00",
+            },
+            {
+                name = "Officer",
+                description = "An active officer of the The Misadventurers League. Contact them if you have any suggestions, problems, etc.",
+                color = "ff9933",
+            },
+            {
+                name = "Guild House",
+                description = "Right click the name in the roster and travel to primary residence",
+                color = "ffcc99",
+            },
+            {
+                name = "Away",
+                description = "Officers have been notified about absence, do not kick",
+                color = "AAAAAA",
+            },
+            {
+                name = "Trial Lead",
+                description = "Leads the trial events",
+                color = "9933ff",
+            },
+            {
+                name = "Event Host",
+                description = "Leads our guild events",
+                color = "00cc00",
+            },
+            {
+                name = "Trial Team",
+                description = "Part of the trial team",
+                color = "cc00cc",
+            },
+        }
+    },
+    {
+        name = "Role",
+        description = "What role(s) do you play? Please contact an officer to have these added.",
+        badges = {
+            {
+                name = "Tank",
+                description = "Pokes the beast and keeps it busy",
+                color = "ff0000",
+            },
+            {
+                name = "Healer",
+                description = "Will keep you alive and kicking",
+                color = "00cc00",
+            },
+            {
+                name = "M DD",
+                description = "Kills all the things with magic",
+                color = "0000ff",
+            },
+            {
+                name = "S DD",
+                description = "Kills all the things with their physical prowess",
+                color = "006600",
+            },
+        }
+    },
+    {
+        name = "Supportive Badges",
+        description = "Do you want to help out your fellow guildies? If so request any of the below from an officer :)",
+        badges = {
+            {
+                name = "Master Crafter",
+                description = "Can craft equipment and items with any trait and level, will also be able to craft enchants, potions and food items",
+                color = "2DFFF8",
+            },
+            {
+                name = "Guild Workshop",
+                description = "Has various service at the primary house, right click their name and travel to primary residence",
+                color = "ffd480"
+            },
+            {
+                name = "Trial Dummy",
+                description = "Has a trial dummy at their primary residence, right click their name and travel to primary residence",
+                color = "800000"
+            },
+            {
+                name = "Decorator",
+                description = "Loves to decorate their houses, right click their name and travel to primary residence",
+                color = "993366"
+            },
+        }
+    },
+})
