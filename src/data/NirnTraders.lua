5789d375eb95cc05fb44d3021cfb808f86a3705b
sirinsidiator <insidiator@cmos.at>
2021-02-04 20:41:24 +0100

    updated data for Nirn Traders

--- src/data/NirnTraders.lua
+++ src/data/NirnTraders.lua
@@ -1,11 +1,24 @@
 AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Nirn Traders", {
     {
-        name = "Trade Badges",
+        name = "Admin Badges",
         badges = {
             {
-                name = "Powerseller",
-                description = "Obtained \"Legend\" Rank 2 weeks in a row.\nWill be removed after obtaining \"Broke AF\" rank 2 weeks in a row.",
-                color = "FFFF00",
+                name = "Guildmaster",
+                color = "DD58F4",
+            },
+            {
+                name = "Aide",
+                description = "Second in Command",
+                color = "007BFF",
+            },
+            {
+                name = "Officer",
+                description = "Guild Officer",
+                color = "F415E9",
+            },
+            {
+                name = "Guild House",
+                color = "B4D3FF",
             },
         },
     },
@@ -13,33 +26,69 @@ AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Nirn Traders
         name = "Support Badges",
         badges = {
             {
-                name = "Friend of NT",
-                description = "Obtained by helping out the guild, can only be given by GM's.",
+                name = "Friend of ATA",
+                description = "Obtained by helping out the guild, can only be given by GM.",
                 color = "7EFDFF",
             },
             {
-                name = "Friend of ATA",
-                description = "Obtained by helping out the guild, can only be given by GM's.",
+                name = "Friend of NT",
+                description = "Obtained by helping out the guild, can only be given by GM.",
                 color = "7EFDFF",
             },
+            {
+                name = "Patron",
+                description = "Considerable Guild Donation/Support",
+                color = "FFA500",
+            },
         }
+    },
+    {
+        name = "Trade Badges",
+        badges = {
+            {
+                name = "Elite Seller",
+                description = "Obtained by being a Top 3 Seller",
+                color = "8E00FF",
+            },
+            {
+                name = "Legend",
+                description = "Obtained \"Legend\" Rank.",
+                color = "FF333D",
+            },
+            {
+                name = "Powerseller",
+                description = "Obtained \"Legend\" Rank 2 weeks in a row.\nWill be removed after obtaining \"Broke AF\" rank 2 weeks in a row.",
+                color = "FFFF00",
+            },
+            {
+                name = "Notification",
+                description = "Weekly sales below requirement + fees not paid.",
+                color = "FFFFFF",
+            },
+        },
     },
     {
         name = "Utility Badges",
         badges = {
             {
-                name = "Officer",
-                description = "Guild officer (stating the obvious here)",
-                color = "FF1493",
+                name = "Master Crafter",
+                description = "Can craft items for coin. Message them for details",
+                color = "75CB84",
             },
             {
-                name = "Warned for Chat Rules",
-                description = "People who broke chat rules - read MOTD for these Rules.",
-                color = "FF0000",
+                name = "Paid Until",
+                description = "Fees paid in advance",
+                color = "FFD95A",
             },
             {
                 name = "Offline Play",
-                description = "People who play on Offline Mode for a longer period of Time. Ask GM for more info.",
+                description = "People who play on Offline Mode for a longer period of Time. Ask GM for more info",
+                color = "009888",
+            },
+            {
+                name = "Vacation",
+                description = "People who are away on holiday + fees paid to cover the time off",
+                color = "7E7E7E",
             },
         },
     },
