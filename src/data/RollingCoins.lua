6164ca67b6aa376aefc8254a81903978d59aede8
sirinsidiator <insidiator@cmos.at>
2021-02-04 20:25:45 +0100

    added data for Flipping Coins, Rolling Coins and Shining Coins

---
+++ src/data/RollingCoins.lua
@@ -0,0 +1,107 @@
+AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Rolling Coins", {
+    {
+        name = "Special Badges",
+        badges = {
+            {
+                name = "Guildmaster",
+                description = "The one who rules them all.",
+                color = "F3FF08",
+            },
+            {
+                name = "Lead",
+                description = "Contestant for the throne.",
+                color = "F7F776",
+            },
+            {
+                name = "Veteran",
+                description = "Former officers/lead.",
+                color = "A1005B",
+            },
+            {
+                name = "Alliance",
+                description = "Part of the ITT Alliance.",
+                color = "2BDEAB",
+            },
+            {
+                name = "Elder",
+                description = "Our most dedicated supporters!",
+                color = "4A60E8",
+            },
+            {
+                name = "Friend",
+                description = "Protected friend.",
+                color = "FFFFFF",
+            },
+        }
+    },
+    {
+        name = "Donation Badges",
+        badges = {
+            {
+                name = "Loyal",
+                description = "200k+ total donation",
+                color = "FF10CC",
+            },
+            {
+                name = "Magnate",
+                description = "100k+ total donation",
+                color = "FF50CC",
+            },
+            {
+                name = "Mogul",
+                description = "50k+ total donation",
+                color = "FF80CC",
+            },
+        }
+    },
+    {
+        name = "Prestige Badges",
+        badges = {
+            {
+                name = "Top Seller",
+                description = "Top 3 Sellers of the week",
+                color = "FF952A",
+            },
+            {
+                name = "Top Buyer",
+                description = "Top 3 Buyers of the week",
+                color = "FFAE70",
+            },
+        }
+    },
+    {
+        name = "Purchase Badges",
+        badges = {
+            {
+                name = "Coinslayer",
+                description = "10x Shopaholics.",
+                color = "AE33FF",
+            },
+            {
+                name = "Shopaholic",
+                description = "1M in weekly purchases.",
+                color = "C46AFF",
+            },
+        }
+    },
+    {
+        name = "Trading Badges",
+        badges = {
+            {
+                name = "God of the Coins",
+                description = "10x Master.",
+                color = "2DFFC3",
+            },
+            {
+                name = "Master",
+                description = "1M+ weekly sales",
+                color = "2DFF99",
+            },
+            {
+                name = "Expert",
+                description = "500k+ weekly sales",
+                color = "2DFF70",
+            },
+        }
+    },
+})
