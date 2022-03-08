1089911aa212109b3e12fd4cb450ed716f749571
sirinsidiator <insidiator@cmos.at>
2022-03-08 16:58:44 +0100

    updated data for Coins guilds

--- src/data/FlippingCoins.lua
+++ src/data/FlippingCoins.lua
@@ -1,107 +1,144 @@
-AetheriusBadgeFilter:RegisterGuild(AetheriusBadgeFilter.SERVER_EU, "Flipping Coins", {
+AetheriusBadgeFilter:RegisterGuild(
+    AetheriusBadgeFilter.SERVER_EU,
+    "Flipping Coins",
     {
-        name = "Special Badges",
-        badges = {
-            {
-                name = "Guildmaster",
-                description = "The one who rules them all.",
-                color = "F3FF08",
-            },
-            {
-                name = "Lead",
-                description = "Contestant for the throne.",
-                color = "F7F776",
-            },
-            {
-                name = "Veteran",
-                description = "Former officers/lead.",
-                color = "A1005B",
-            },
-            {
-                name = "Alliance",
-                description = "Part of the ITT Alliance.",
-                color = "2BDEAB",
-            },
-            {
-                name = "Elder",
-                description = "Our most dedicated supporters!",
-                color = "4A60E8",
-            },
-            {
-                name = "Friend",
-                description = "Protected friend.",
-                color = "FFFFFF",
-            },
+        {
+            name = "Special Badges",
+            badges = {
+                {
+                    name = "Guildmaster",
+                    description = "The one who rules them all.",
+                    color = "F3FF08"
+                },
+                {
+                    name = "Council",
+                    description = "Second only to the |cF3FF08Guildmaster|r.",
+                    color = "F7F776"
+                },
+                {
+                    name = "Doyen",
+                    description = "Part of the ITT Alliance and former Officers/Leads",
+                    color = "A1005B"
+                },
+                {
+                    name = "Elder",
+                    description = "Our most dedicated supporters!",
+                    color = "4A60E8"
+                },
+                {
+                    name = "Friend",
+                    description = "Protected friend.",
+                    color = "fffdd0"
+                },
+                {
+                    name = "Eternal",
+                    description = "Can only be kicked through inactivity!",
+                    color = "2BDEAB"
+                }
+            }
+        },
+        {
+            name = "Prestige Badges",
+            badges = {
+                {
+                    name = "Partisan",
+                    description = "Awarded to a very select individual who has been |cB45F04incredibly supportive|r to the guild or has helped the guild in an |cB45F04extraordinary way|r! This is the |cB45F04highest honor|r one can receive in this guild. ",
+                    color = "B45F04"
+                },
+                {
+                    name = "Champion",
+                    description = "Has currently the |cb8860bhighest sales in one week of any individual ever|r. This badge can only be given to one player at a time. Once the record is broken this badge will be given to the new record holder. ",
+                    color = "b8860b"
+                },
+                {
+                    name = "Top Seller",
+                    description = "Has been one of the |cA9E2F33 Top-Traders|r in one trading week. Can be awarded multiple times.",
+                    color = "AE33FF",
+                    limit = 999
+                },
+                {
+                    name = "Top Buyer",
+                    description = "Has been one of the |cA9E2F33 Top-Buyers|r in one trading week. Can be awarded multiple times.",
+                    color = "C46AFF",
+                    limit = 999
+                }
+            }
+        },
+        {
+            name = "Trading Badges",
+            badges = {
+                {
+                    name = "Diamond Trader",
+                    description = "Reached |c3EAED01 Million sales|r in one trading week. After reaching 10 of these badges, will turn into |c4997D0God of Sales|r",
+                    color = "3EAED0",
+                    limit = 10,
+                    turnsInto = "God of Sales"
+                },
+                {
+                    name = "God of Sales",
+                    description = "Has been 10 times |c3EAED0Diamond Trader|r. Can be awarded multiple times.",
+                    color = "4997D0",
+                    limit = 999
+                },
+                {
+                    name = "Emerald Trader",
+                    description = "Reached |c50C8785 Million|r sales in one trading week. After reaching 10 of these badges, will turn into |c3B7A57Saint|r!",
+                    color = "50C878",
+                    limit = 10,
+                    turnsInto = "Saint"
+                },
+                {
+                    name = "Saint",
+                    description = "Has been 10 times |c50C878Emerald Trader|r. Can be awarded multiple times.",
+                    color = "3B7A57",
+                    limit = 999
+                },
+                {
+                    name = "Ruby Trader",
+                    description = "Reached |cF6221710 Million|r sales in one trading week. After reaching 10 of these badges, will turn into |c660000Mogul|r! \n\nAdded August 2021",
+                    color = "F62217",
+                    limit = 10,
+                    turnsInto = "Mogul"
+                },
+                {
+                    name = "Mogul",
+                    description = "Has been 10 times |cF62217Ruby Trader|r. Can be awarded multiple times.",
+                    color = "660000",
+                    limit = 999
+                }
+            }
+        },
+        {
+            name = "Donation Badges",
+            badges = {
+                {
+                    name = "Supporter",
+                    description = "Donated over |cFE2EC850k.|r",
+                    color = "FE2EC8",
+                    limit = 2,
+                    turnsInto = "ArchSupporter"
+                },
+                {
+                    name = "ArchSupporter",
+                    description = "Donated over |cFA58AC100k.|r",
+                    color = "FA58AC",
+                    limit = 5,
+                    turnsInto = "Patron"
+                },
+                {
+                    name = "Patron",
+                    description = "Donated over |c5FB404500k.|r",
+                    color = "5FB404",
+                    limit = 2,
+                    turnsInto = "ArchPatron"
+                },
+                {
+                    name = "ArchPatron",
+                    description = "Donated over |c088A081M.|r",
+                    color = "088A08",
+                    limit = 999
+                }
+            }
         }
-    },
-    {
-        name = "Donation Badges",
-        badges = {
-            {
-                name = "Loyal",
-                description = "200k+ total donation",
-                color = "FF10CC",
-            },
-            {
-                name = "Magnate",
-                description = "100k+ total donation",
-                color = "FF50CC",
-            },
-            {
-                name = "Mogul",
-                description = "50k+ total donation",
-                color = "FF80CC",
-            },
-        }
-    },
-    {
-        name = "Prestige Badges",
-        badges = {
-            {
-                name = "Top Seller",
-                description = "Top 3 Sellers of the week",
-                color = "FF952A",
-            },
-            {
-                name = "Top Buyer",
-                description = "Top 3 Buyers of the week",
-                color = "FFAE70",
-            },
-        }
-    },
-    {
-        name = "Purchase Badges",
-        badges = {
-            {
-                name = "Coinslayer",
-                description = "10x Shopaholics.",
-                color = "AE33FF",
-            },
-            {
-                name = "Shopaholic",
-                description = "1M in weekly purchases.",
-                color = "C46AFF",
-            },
-        }
-    },
-    {
-        name = "Trading Badges",
-        badges = {
-            {
-                name = "God of the Coins",
-                description = "10x Master.",
-                color = "2DFFC3",
-            },
-            {
-                name = "Master",
-                description = "1M+ weekly sales",
-                color = "2DFF99",
-            },
-            {
-                name = "Expert",
-                description = "500k+ weekly sales",
-                color = "2DFF70",
-            },
-        }
-    },
-})
+    }
+)
